250e8c6e9b34e23c088dc9f79378ce899b753cb7
takeru.hashimoto <hstake25@gmail.com>
2023-05-12 01:01:05 +0900

    3-17

---
+++ 3-17.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m["apple"])
+	fmt.Println(m["banana"])
+
+	m["banana"] = 300
+	fmt.Println(m["banana"])
+
+	m["new"] = 500
+	fmt.Println(m)
+
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	var m3 map[string]int
+	// m3 は nil なので append できない
+	m3["pc"] = 5000
+	fmt.Println(m3)
+}
