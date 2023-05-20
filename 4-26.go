f6f3243826fdd55ec31802baa55a02b628654a39
takeru.hashimoto <hstake25@gmail.com>
2023-05-20 17:54:21 +0900

    4-26

---
+++ 4-26.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	l := []string{"a", "b", "c"}
+
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
+	}
+
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	m := map[string]int{"a": 100, "b": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	for _, v := range m {
+		fmt.Println(v)
+	}
+
+}
