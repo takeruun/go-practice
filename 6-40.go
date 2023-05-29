72962641aa3efc8ac30cfbb60eecba5bc03fe615
takeru.hashimoto <hstake25@gmail.com>
2023-05-29 21:17:26 +0900

    6-40

---
+++ 6-40.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	x, y int
+}
+
+func (v Vertex) Area() int {
+	return v.x * v.y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+func Area(v Vertex) int {
+	return v.x * v.y
+}
+
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
+}
+
+func main() {
+	v := New(3, 4)
+	v.Scale(10)
+
+	fmt.Println(v.Area())
+}
