2c07386964c1776222a6c019ebbda3f6754082cd
takeru.hashimoto <hstake25@gmail.com>
2023-06-15 22:56:02 +0900

    7-56

---
+++ 7-56.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+
+	// OuterLoop を使用する、break で抜けれる
+OuterLoop:
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			break OuterLoop
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+
+	fmt.Println("end")
+
+}
