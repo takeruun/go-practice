958360bd08724166885ea039b570d5483ff4a572
takeru.hashimoto <hstake25@gmail.com>
2023-06-17 23:59:28 +0900

    7-57

---
+++ 7-57.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	// c := make(map[string]int)
+
+	// 同時に書き込みが行われると、データの破壊が起きる
+	// fatal error: concurrent map writes
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		c["key"]++
+	// 	}
+	// }()
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		c["key"]++
+	// 	}
+	// }()
+
+	c := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("key")
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("key")
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(c, c.Value("key"))
+
+}
