db336bb7901732f2e43428b075981dcb5ca918d5
takeru.hashimoto <hstake25@gmail.com>
2023-06-27 22:56:58 +0900

    9-70

---
+++ 9-70.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const (
+	c1 = iota
+	c2 = iota
+	c3 = iota
+	// c1 = iota
+	// c2
+	// c3
+)
+
+const (
+	_      = iota
+	KB int = 1 << (10 * iota)
+	MB
+	GB
+)
+
+func main() {
+	fmt.Println(c1, c2, c3)
+
+	fmt.Println(KB, MB, GB)
+}
