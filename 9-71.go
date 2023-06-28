3f336355b1729ac76014a250ac1f6614b0c7a3eb
takeru.hashimoto <hstake25@gmail.com>
2023-06-28 23:27:27 +0900

    9-71

---
+++ 9-71.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
+
+func main() {
+	ch := make(chan string)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	// cancel()
+	// ctx := context.TODO() // 特に何もしない場合は TODO() を使う
+	go longProcess(ctx, ch)
+
+CTXLOOP:
+	for {
+		fmt.Println("for")
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break CTXLOOP
+		case <-ch:
+			fmt.Println("success")
+			break CTXLOOP
+		}
+	}
+	fmt.Println("#################")
+}
